20ba3dbc0b84ab34c8bb57e031ab266f14ff8265
Muhammad Irvansyah <mirvansyah132@gmail.com>
2025-12-07 15:32:20 +0700

    user

--- database/migrations/2025_12_07_080734_create_tempat_wisatas_table.php
+++ database/migrations/2025_12_07_080734_create_tempat_wisatas_table.php
@@ -4,24 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('tempat_wisatas', function (Blueprint $table) {
+        Schema::create('tempat_wisata', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_tempat');
+            $table->text('deskripsi')->nullable();
+
+            $table->foreignId('kategori_id')
+                  ->constrained('kategori_wisata')
+                  ->onDelete('cascade');
+
+            $table->foreignId('kota_id')
+                  ->constrained('kota')
+                  ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('tempat_wisatas');
+        Schema::dropIfExists('tempat_wisata');
     }
 };
